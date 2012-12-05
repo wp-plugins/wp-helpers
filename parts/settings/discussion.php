056e0e40662cd219600e5f60ba53548c83b217c5
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-05 03:10:32 +0000

    v1.4.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-helpers/tags/1.4.3@634252 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ parts/settings/discussion.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Title: Comments
+Setting: piklist_wp_helpers
+Tab: Discussion
+Order: 50
+*/
+
+  piklist('field', array(
+    'type' => 'checkbox'
+    ,'field' => 'comments_open_pages'
+    ,'label' => 'Pages'
+    ,'choices' => array(
+      'true' => 'Do not allow comments on Pages'
+    )
+  ));
+
+  piklist('field', array(
+    'type' => 'checkbox'
+    ,'field' => 'make_clickable'
+    ,'label' => 'Automatic linking'
+    ,'choices' => array(
+      'true' => 'Turn off automatic linking of urls in comments'
+    )
+  ));
+
+  piklist('shared/built-with-piklist', array(
+    'location' => __FILE__
+  ));
+?>
