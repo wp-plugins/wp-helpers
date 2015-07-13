09b6398abec026605ee98b53cbf02cbb290b3e16
sbruner <sbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 19:29:20 +0000

    update to version 1.8
    git-svn-id: https://plugins.svn.wordpress.org/wp-helpers/tags/1.8@1198247 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ parts/settings/media-image-sizes.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Title: Sizes
+Setting: piklist_wp_helpers
+Tab: Media
+Order: 700
+Tab Order: 70
+*/
+
+  piklist('field', array(
+    'type' => 'checkbox'
+    ,'field' => 'show_additional_image_sizes'
+    ,'label' => 'Image sizes'
+    ,'help' => 'Once selected, edit any image in your Media Library to see additional image information'
+    ,'list' => true
+    ,'choices' => array(
+      'true' => 'Show sizes and urls on image edit screen'
+    )
+  ));
